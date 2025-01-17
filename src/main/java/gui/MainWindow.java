a47c2475f751e5f4f127453e0f1fe3a18b0d911f
Mohammad <170977627+itsMoha963@users.noreply.github.com>
2025-01-17 18:00:39 +0100

    icon support for  macOS and some minor changes here and there

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -4,11 +4,13 @@ import gui.view.DroneCatalog;
 import gui.view.DroneDashboard;
 import gui.view.FlightDynamics;
 import services.DroneSimulationInterfaceAPI;
-import utils.Constants;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class MainWindow extends JFrame {
     private DroneSimulationInterfaceAPI droneAPI;
@@ -18,15 +20,30 @@ public class MainWindow extends JFrame {
         setSize(900, 1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        try {
-            setIconImage(ImageIO.read(getClass().getResource(Constants.APP_ICON_PATH)));
-        } catch (IOException e) {
-            System.err.println("Failed to load App Icon: " + e.getMessage());
-        }
+        // Set application icon dynamically
+        setAppIcon();
 
         createTaskBar();
     }
 
+    private void setAppIcon() {
+        try {
+            // Dynamically locate the icon within resources
+            URL iconURL = getClass().getResource("/Icons/appicon.png");
+            if (iconURL != null) {
+                setIconImage(ImageIO.read(iconURL));
+                // Special case for macOS to set the Dock icon
+                if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                    Taskbar taskbar = Taskbar.getTaskbar();
+                    taskbar.setIconImage(ImageIO.read(iconURL));
+                }
+            } else {
+                System.err.println("App icon not found at the specified path!");
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to load App Icon: " + e.getMessage());
+        }
+    }
     public void createTaskBar() {
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.addTab("Drone Catalog", new DroneCatalog());
